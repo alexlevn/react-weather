c5278f0a3bc441d6a6bf46901c3b9080693d737f
thanhdungfb <thanhdungfb@gmail.com>
2018-04-03 22:10:57 +0700

    added copy

--- app/api/openWeatherMap.jsx
+++ app/api/openWeatherMap.jsx
@@ -1,10 +1,6 @@
 var axios = require('axios');
 
 const OPEN_WEATHER_MAP_URL = 'http://api.openweathermap.org/data/2.5/weather?appid=837a333216fd7fe79c96d6fd4db60b31&units=imperial';
-
-// const OPEN_WEATHER_MAP_URL = 'http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=837a333216fd7fe79c96d6fd4db60b31&units=imperial';
-// Tạo Tài khoản + Tạo 1 key: 837a333216fd7fe79c96d6fd4db60b31
-
 module.exports = {
     getTemp: function(location){
         var encodeLocation = encodeURIComponent(location);
@@ -25,9 +21,6 @@ module.exports = {
             // debugger;                  
             // throw new Error(res.data.message);
             throw new Error(res.response.data.message);
-            // Axios phiên bản 0.18 khác 0.9 => dùng debug để biết kết quả trả về.
-
-            // Làm đến bài 035. Làm tiếp sau
         });
     }
 }
