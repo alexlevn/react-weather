fff7eb23224b4b16b07dd7e9f970a19913c60fa5
thanhdungfb <thanhdungfb@gmail.com>
2018-04-03 22:15:36 +0700

    added copyy

--- app/api/openWeatherMap.jsx
+++ app/api/openWeatherMap.jsx
@@ -1,26 +1,24 @@
 var axios = require('axios');
 
-const OPEN_WEATHER_MAP_URL = 'http://api.openweathermap.org/data/2.5/weather?appid=837a333216fd7fe79c96d6fd4db60b31&units=imperial';
+const OPEN_WEATHER_MAP_URL = 'http://api.openweathermap.org/data/2.5/weather?appid=837a333216fd7fe79c96d6fd4db' +
+        '60b31&units=imperial';
 module.exports = {
-    getTemp: function(location){
+    getTemp: function (location) {
         var encodeLocation = encodeURIComponent(location);
         var requestUrl = `${OPEN_WEATHER_MAP_URL}&q=${encodeLocation}`;
         // debugger;
-        return axios.get(requestUrl).then(function(res){
-            // Success
-            // alert(res.cod);
-            debugger;
-            
-            if(res.data.cod && res.data.message){
-                throw new Error(res.data.message);
-            }else{
-                return res.data.main.temp;
-
-            }
-        }, function(res){      
-            // debugger;                  
-            // throw new Error(res.data.message);
-            throw new Error(res.response.data.message);
-        });
+        return axios
+            .get(requestUrl)
+            .then(function (res) {
+                // Success alert(res.cod); debugger;
+                if (res.data.cod && res.data.message) {
+                    throw new Error(res.data.message);
+                } else {
+                    return res.data.main.temp;
+                }
+            }, function (res) {
+                // debugger;
+                throw new Error(res.response.data.message);
+            });
     }
 }
